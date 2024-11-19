692ca87a701aeda892b195c3f64e702f7c78e9d9
Mikael Odair Klein <mikaeloklein@gmail.com>
2024-11-19 16:45:19 -0300

    a

--- CRUD_RESIDUOS/listarResiduo.php
+++ CRUD_RESIDUOS/listarResiduo.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de Resíduos</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <header>
