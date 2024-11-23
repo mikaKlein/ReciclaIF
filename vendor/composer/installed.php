703cddc78396031bb8a96a86b13bd757f1acd8d0
Mikael <mikaeloklein@gmail.com>
2024-11-23 11:17:43 -0300

    No Git Ignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mikae/recicla-if',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '692ca87a701aeda892b195c3f64e702f7c78e9d9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mikae/recicla-if' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '692ca87a701aeda892b195c3f64e702f7c78e9d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
